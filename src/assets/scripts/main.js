a80c1e5920a521bb034ec47d57655ab2228a6f19
Rubén Martín Bravo <rmartinbra@uoc.edu>
2024-05-08 17:35:27 +0200

    Final code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,12 +9,7 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
-
-
+/** FORMULARIO **/
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const age = document.getElementById('age');
@@ -22,7 +17,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Show input error message
+
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
@@ -30,77 +25,69 @@ function showError(input, message) {
     small.innerText = message;
 }
 
-// Show success outline
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
 
-// Check email is valid
 function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test(input.value.trim())) {
         showSuccess(input);
     } else {
-        showError(input, 'Email is not valid');
+        showError(input, 'El correo electrónico no es válido');
     }
 }
 
-// Check required fields
 function checkRequired(inputArr) {
    inputArr.forEach(function(input) {
         if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
+            showError(input, `${getFieldName(input)} es obligatorio`);
         }   else {
             showSuccess(input);
         }
    }); 
 }
 
-// Check input length
 function checkLength(input, min, max) {
     if (input.value.length < min) {
-            showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+            showError(input, `${getFieldName(input)} debe tener al menos ${min} caracteres`);
         }   else if (input.value.length > max) {
-            showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+            showError(input, `${getFieldName(input)} debe tener menos de ${max} caracteres`);
         }   else {
             showSuccess(input);
         }
 }
 
-// Check passwords match
 function checkPasswordsMatch(input1, input2) {
     if(input1.value !== input2.value ) {
-        showError(input2, 'Passwords do not match');
+        showError(input2, 'Las contraseñas no coinciden');
     }
 }
 
-// Check if age is valid
 function checkAge(input) {
     const ageValue = parseInt(input.value);
     if (isNaN(ageValue) || ageValue < 0 || ageValue >= 100) {
-        showError(input, 'Age must be between 0 and 99');
+        showError(input, 'La edad debe estar entre 0 y 99 años');
     } else {
         showSuccess(input);
     }
 }
 
-// Check if the password is complex and therefore more secure
 function checkPasswordSecurity(input) {
     const password = input.value;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[`~!@#$%^&*()_+\-=\{\}\[\]|:;'"<>,.?/\\]).{8,}$/;
     if (!passwordRegex.test(password)) {
-        showError(input, 'Password must contain at least 8 characters, including uppercase, lowercase, digits, and special characters: ` ~ ! @ # $ % ^ & * ( ) _ + - = { } | [ ] \\ : " ; \' < > ? , . /');
+        showError(input, 'La contraseña debe constar de al menos 8 caracteres, including mayúsculas, minúsculas, dígitos, y caracteres especiales: ` ~ ! @ # $ % ^ & * ( ) _ + - = { } | [ ] \\ : " ; \' < > ? , . /');
     } else {
         showSuccess(input);
     }
 }
-// Get fieldName
+
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);  
 }
 
-// Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
